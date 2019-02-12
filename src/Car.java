4a090284c7de55ea223fed0554302a174739d16e
Orirane <worldofcrimson@yahoo.com>
2019-02-12 22:38:23 +0300

    hmmm

--- src/Car.java
+++ src/Car.java
@@ -1,65 +1,62 @@
 public class Car {
- private String model;
- private int location;
- private int currentSpeed;
- private boolean movingForward;
- private int maxSpeed;
+    private String model;
+    private int location;
+    private int currentSpeed;
+    private boolean movingForward;
+    private int maxSpeed;
+    private boolean changedDirection = false;
  
- // constructor creates a car object
- public Car (String model, int maxSpeed, int location){
-   this.model = model;
-   this.maxSpeed = maxSpeed;
-   this.location = location;
-   this.movingForward = true;
-   this.currentSpeed = 0;
- }
- //getters
- public String getModel(){
-   return this.model;
- }
+    // constructor creates a car object
+    Car (String model, int maxSpeed, int location){
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.location = location;
+        this.movingForward = true;
+        this.currentSpeed = 0;
+    }
+    //getters
+    public String getModel(){
+        return this.model;
+    }
  
- public boolean getDirection(){
-   return this.movingForward;
- }
+    boolean getDirection(){
+        return this.movingForward;
+    }
  
- public int getLocation(){
-    return this.location;
- }
-//method sets the current speeed of car to be the object maxSpeed
-   public int go(){
-     return currentSpeed = maxSpeed;  
-   }
+    int getLocation(){
+        return this.location;
+    }
+//method sets the current speed of car to be the object maxSpeed
+void go(){
+    this.currentSpeed = this.maxSpeed;
+
+}
 //method sets the current speed of the car to 0
-   public int stop(){
-       return currentSpeed = 0;
-   }
-   public boolean turnAround(){
-     if (this.movingForward){
-       this.movingForward = false;
-     }else{
-         this.movingForward = true;
-       }
-     return movingForward;
-   }
+public int stop(){
+    return currentSpeed = 0;
+}
+    void turnAround(){
+        this.movingForward = !this.movingForward;
+        changedDirection = true;
+    }
 //change the location: add or subtract the currentSpeed from the location
-   public int continu(){
-     if (movingForward = true){
-       this.location += this.currentSpeed;
-     }
-     if (movingForward = false) {
-       this.location -= this.currentSpeed;
-     }
-     return this.location;
-   }
-   public void display(){
-     String temp = "";
-     if (movingForward = true) {
-       temp = "forward"; 
-     }else if (movingForward = false) {
-       temp = "backwards";
-     }
-       System.out.println(this.model + ": Located at " + this.location + 
-               ", fasing " + temp + " and moving at " + this.currentSpeed + " speed.");
-   }
-  
+void cont(){
+    if (movingForward){
+        this.location += this.currentSpeed;
+    }
+    if (!movingForward) {
+        this.location -= this.currentSpeed;
+    }
+}
+    void display(){
+        String temp = "";
+        if (movingForward) {
+            temp = "forward";
+        } else if (!movingForward) {
+            temp = "backwards";
+        }
+        System.out.println(this.model + ": Located at " + this.location +
+                ", facing " + temp + " and moving at " + this.currentSpeed + " speed.");
+    }
+
 }
--- src/RaceLane.java
+++ src/RaceLane.java
@@ -1,75 +1,80 @@
-import java.util.*;
+import java.util.Scanner;
 public class RaceLane {
+
  
-  public static void main(String[] args){
-    Scanner reader = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        boolean car1Left = false;
+        boolean car2Left = false;
+        boolean carsCollide = false;
     
-  //ask user for an input
-    System.out.println("Model: ");
+        //ask user for an input
+        System.out.println("Model: ");
     String model = reader.next();
     System.out.println("Location: ");
     int location = reader.nextInt();
     System.out.println("Max speed: ");
     int maxSpeed = reader.nextInt();
-    
-  //create the first car object
+        /*String model = "Camry";
+        int location = 0;
+        int maxSpeed = 11;*/
+        //create the first car object
         Car car1 = new Car(model, maxSpeed, location);
   
-  // ask user for input and do a second car object
-        
+        // ask user for input and do a second car object
+
      System.out.println("Model: ");
      String model2 = reader.next();
      System.out.println("Location: ");
      int location2 = reader.nextInt();
      System.out.println("Max speed: ");
      int maxSpeed2 = reader.nextInt();
+
+       /* String model2 = "Corolla";
+        int location2 = 100;
+        int maxSpeed2 = 10;*/
+        Car car2 = new Car(model2, maxSpeed2, location2);
      
-     Car car2 = new Car(model2, maxSpeed2, location2);
+        //display both cars
+        car1.display();
+        car2.display();
      
-  //display both cars 
-     car1.display();
-     car2.display();
-     
-  //determine wich one is on the left   
-     if (car1.getLocation()<car2.getLocation()){
+        //determine which one is on the left
+        if (car1.getLocation()<car2.getLocation()){
             System.out.println(model +  " is to the left of " + model2);
-            car1.turnAround();
+            car2.turnAround();
+            car1Left = true;
             System.out.println(car1.getDirection());
         }
-     if (car2.getLocation()<car1.getLocation()){
+        if (car2.getLocation()<car1.getLocation()){
             System.out.println(model2 +  " is to the left of " + model);
-            car2.turnAround();
+            car1.turnAround();
+            car2Left = true;
             System.out.println(car2.getDirection());
         }
         car1.display();
         car2.display();  
-  //makes cars move   
-     car1.go();
-     car2.go();
+        //makes cars move
+        car1.go();
+        car2.go();
      
-     car1.display();
-     car2.display(); 
+        car1.display();
+        car2.display();
      
-     System.out.println(car1.getDirection());
-     System.out.println(car2.getDirection());
+        System.out.println(car1.getDirection());
+        System.out.println(car2.getDirection());
      
    
-  //makes cars collide
-    while (car1.getDirection() && car2.getDirection()){
+        //makes cars collide
+        while (!carsCollide){
+            car1.cont();
+            car2.cont();
+            car1.display();
+            car2.display();
+            if(car1Left && car1.getLocation() >= car2.getLocation() )carsCollide = true;
+            else if(car2Left && car1.getLocation() <= car2.getLocation()) carsCollide = true;
+        }
      
-      if (car1.getLocation() - car2.getLocation() < 0){
-       car1.turnAround();
-       car1.display();
-       car2.display();
-      }else if (car1.getLocation() - car2.getLocation() > 0){
-       car2.turnAround();
-       car1.display();
-       car2.display();
+        System.out.println ("Boom!");
     }
-       car1.continu();
-       car2.continu();
-    } 
-     
-        System.out.println ("Boom!"); 
-   }    
 }
